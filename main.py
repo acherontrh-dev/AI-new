58e1ee4a583530e0df4b64eddb8eadfd470ee670
acherontrh-dev <acherontrh@gmail.com>
2026-03-08 11:58:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class ChatRequest(BaseModel):
-    prompt: str
-
 @app.get("/")
 def home():
-    return {"status":"AI running"}
+    return {"status": "server running"}
 
-@app.post("/chat")
-def chat(req: ChatRequest):
-
-    return {
-        "response":"AI reply: " + req.prompt
-    }
+@app.get("/health")
+def health():
+    return {"ok": True}
